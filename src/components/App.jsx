9dca1133efbe8909b0d88338f64a54c09aa29e8c
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-14 09:00:38 +0100

    Add reuseanle Avatar component to App component. Final commit.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://4.bp.blogspot.com/-Rju0s2G-ybg/WcWDFxng1MI/AAAAAAAAFGE/yrrOnzBXtLUyR9cw2QZHm68_QrGeseqTwCLcBGAs/s1600/Rotary_Phone_Red_for%2B_RJ.jpg" />
 
       <Card
         name={contacts[0].name}
